158242305742122460344d17c042a5973425d638
frizzle-chan <frizzle@frizzle.wtf>
2021-10-23 14:18:16 -0500

    Initial GlowStickVendingMachine import

---
+++ GlowStickVendingMachine/Scripts/glowstickbutton.cs
@@ -0,0 +1,27 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.SDK3.Components;
+
+public class glowstickbutton : UdonSharpBehaviour
+{
+    public VRCObjectPool pool;
+
+    public Transform spawnPos;
+
+    public override void Interact()
+    {
+        Networking.SetOwner(Networking.LocalPlayer, pool.gameObject);
+
+        var stick = pool.TryToSpawn();
+
+        if (stick == null)
+        {
+            return;
+        }
+
+        Networking.SetOwner(Networking.LocalPlayer, stick);
+        stick.gameObject.transform.SetPositionAndRotation(spawnPos.position, spawnPos.rotation);
+    }
+}
---
+++ GlowStickVendingMachine/Scripts/glowstickclean.cs
@@ -0,0 +1,59 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common;
+using VRC.SDK3.Components;
+
+public class glowstickclean : UdonSharpBehaviour
+{
+    public bool dropped = true;
+
+    private int gcTimeoutSeconds = 60;
+    private void Start()
+    {
+        dropped = true;
+    }
+
+    public override void OnPickup()
+    {
+        dropped = false;
+    }
+
+    public override void OnDrop()
+    {
+        _Drop();
+    }
+
+    public void _Drop()
+    {
+        dropped = true;
+        SendCustomEventDelayedSeconds(nameof(_CleanStick), gcTimeoutSeconds);
+    }
+
+    public override void OnOwnershipTransferred(VRCPlayerApi player)
+    {
+        var pickup = (VRCPickup)GetComponentInParent(typeof(VRCPickup));
+        if (pickup.IsHeld)
+        {
+            return;
+        }
+
+        _Drop();
+    }
+
+    public void _CleanStick()
+    {
+        if (!dropped)
+        {
+            return;
+        }
+
+        var parent = (VRCObjectPool)GetComponentInParent(typeof(VRCObjectPool));
+        Networking.SetOwner(Networking.LocalPlayer, parent.gameObject);
+        parent.Return(this.gameObject);
+
+    }
+
+}
