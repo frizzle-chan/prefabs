bd2a58cc7d2e333fa9d27e74344b288812f37e14
frizzle-chan <frizzle@frizzle.wtf>
2021-10-25 23:44:07 -0500

    Master controlled vending machine

--- GlowStickVendingMachine/Scripts/glowstickbutton.cs
+++ GlowStickVendingMachine/Scripts/glowstickbutton.cs
@@ -6,22 +6,52 @@ using VRC.SDK3.Components;
 
 public class glowstickbutton : UdonSharpBehaviour
 {
-    public VRCObjectPool pool;
 
-    public Transform spawnPos;
+    private VRCObjectPool pool;
+    private VRCPickup[] glowstickPickups;
+    private VRCPlayerApi player;
+    private void Start()
+    {
+        player = Networking.LocalPlayer;
+        pool = (VRCObjectPool)GetComponentInParent(typeof(VRCObjectPool));
+        glowstickPickups = new VRCPickup[pool.Pool.Length];
+        for (int i = 0; i < glowstickPickups.Length; i++)
+        {
+            glowstickPickups[i] = (VRCPickup)pool.Pool[i].GetComponent(typeof(VRCPickup));
+        }
+    }
 
     public override void Interact()
     {
-        Networking.SetOwner(Networking.LocalPlayer, pool.gameObject);
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.Owner, "SpawnGlowStick");
+    }
 
-        var stick = pool.TryToSpawn();
-
-        if (stick == null)
+    public void SpawnGlowStick()
+    {
+        if (!player.IsOwner(pool.gameObject))
         {
             return;
         }
 
-        Networking.SetOwner(Networking.LocalPlayer, stick);
-        stick.gameObject.transform.SetPositionAndRotation(spawnPos.position, spawnPos.rotation);
+        // clean up unused glow sticks
+        foreach(VRCPickup pickup in glowstickPickups)
+        {
+            if (pickup.gameObject.activeSelf && !pickup.IsHeld)
+            {
+                pool.Return(pickup.gameObject);
+            }
+        }
+
+
+        var stick = pool.TryToSpawn();
+        if (!Utilities.IsValid(stick))
+        {
+            return;
+        }
+
+        // set glow stick on its side
+        var gsRotation = pool.transform.rotation * Quaternion.Euler(0, 0, 90); 
+
+        stick.gameObject.transform.SetPositionAndRotation(pool.transform.position, gsRotation);
     }
 }
